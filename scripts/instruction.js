76ed534884cf732cf24e3c918947debfc09a06b7
Avnoor Rocks <noorbhangu28@gmail.com>
2022-03-12 07:02:51 +0530

    Avnoor added instructions.html and javascript

---
+++ scripts/instruction.js
@@ -0,0 +1,29 @@
+
+function confirmInstructions() {
+    console.log("in")
+    let Instructions = document.getElementById("instructions").value;
+    console.log(Instructions);
+
+    firebase.auth().onAuthStatedChanged(user => {
+        if(user) {
+            var currentUser = db.collection("users").doc(user.uid)
+            var userID = user.uid;
+            
+            currentUser.get()
+                .then(userDoc => {
+                    var userEmail = userDoc.data().email;
+                    db.collection("instructions").add({
+                        userID: userID,
+                        Instructions: Instructions
+
+                    }).then(() => {
+                        window.location.href = "main.html";
+                    })
+                })
+
+        } else {
+            // No user is signed in.
+        }
+    });
+
+}
