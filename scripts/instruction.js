0015ca4e89420cfc305244f6118fed0e122d92af
Avnoor Rocks <noorbhangu28@gmail.com>
2022-03-23 04:04:04 +0530

    Avnoor edited prsonalinfo.html and personal.css

--- scripts/instruction.js
+++ scripts/instruction.js
@@ -1,10 +1,37 @@
+var currentUser
+function populateInfo() {
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if user is signed in:
+        if (user) {
+
+            //go to the correct user document by referencing to the user uid
+            currentUser = db.collection("users").doc(user.uid)
+            //get the document for current user.
+            currentUser.get()
+                .then(userDoc => {
+                    //get the data fields of the user
+                    var Instructions = userDoc.data().instructions;
+                
+                    //if the data fields are not empty, then write them in to the form.
+                    if (Instructions != null) {
+                        document.getElementById("instructions").value = Instructions;
+                    }
+                })
+                
+        } else {
+            // No user is signed in.
+            console.log ("No user is signed in");
+        }
+    });
+}
+populateInfo();
 
 function confirmInstructions() {
     console.log("in")
     let Instructions = document.getElementById("instructions").value;
     console.log(Instructions);
 
-    firebase.auth().onAuthStatedChanged(user => {
+    firebase.auth().onAuthStateChanged(user => {
         if(user) {
             var currentUser = db.collection("users").doc(user.uid)
             var userID = user.uid;
