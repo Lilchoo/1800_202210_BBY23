05316d607edaedb7d0dbdc794536ac4699b0b19c
Linh Nguyen <knguyen142@my.bcit.ca>
2022-03-24 23:29:44 -0700

    Linh modified thank-you message after survey submission; changed navbar and footer color; fixed some css styles

--- scripts/survey.js
+++ scripts/survey.js
@@ -36,17 +36,27 @@ function surveyResults() {
                         timestamp: firebase.firestore.FieldValue.serverTimestamp()
                     }).then(() => {
                         //A thank-you message shows up
-                        str = "<h1 class='' id='profile-title' style='color: #F74F20'>Thanks for your submission! Let's see your result now.</h1>";
-                        document.getElementById("after-submit").innerHTML = str;
+                        let div = document.getElementById("survey-container");
+
+                        str = "<div id='message-after-submission'><h1 class='mx-3 my-5 text-center'>Thanks for your submission!</h1>"
+                        + "<div class='d-grid gap-2 d-sm-flex justify-content-sm-center text-center w-75 mx-auto'>"
+                        + "<button type='button' class='btn btn-lg btn-secondary mx-auto my-3' style='background-color: ; max-width: fit-content; font-size: 25px'><a href='./survey.html'"
+                        + "class='text-light text-decoration-none'>Survey again</a></button>"
+                        + "<button type='button' class='btn btn-lg mx-auto my-3' style='background-color: #F74F20; max-width: fit-content; font-size: 25px'><a href='./personalHealth.html'"
+                        + "class='text-light text-decoration-none'>See Result</a></button>"
+                        + "<button type='button' class='btn btn-lg mx-auto my-3 btn-success' style='max-width: fit-content; font-size: 25px'><a href='./main.html'"
+                        + "class='text-light text-decoration-none'>Back To Home</a></button></div></div>"
+                        + "<div id='thank-you-image'><img src='/images/thank-you.jpg' width='100%'/></div>";
                         
+                        div.innerHTML = str;
                     }).then(() => {
                         //Navigates user to the main page after 5 sec
-                        var intID = setInterval(function () {
-                            window.location.assign("main.html");
-                        }, 5000);
-                        setTimeout(function () {
-                            clearInterval(intID);
-                        }, 5000);
+                        // var intID = setInterval(function () {
+                        //     window.location.assign("main.html");
+                        // }, 5000);
+                        // setTimeout(function () {
+                        //     clearInterval(intID);
+                        // }, 5000);
                     })
                         
                 })
