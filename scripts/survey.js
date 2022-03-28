a82bb51374b9651c90162cb601a51eecdfe31936
Linh Nguyen <knguyen142@my.bcit.ca>
2022-03-27 23:35:41 -0700

    Linh added 2 functions: (1) ensure users do survey once a day, (2) do survey again

--- scripts/survey.js
+++ scripts/survey.js
@@ -1,8 +1,9 @@
-function surveyResults() {
+// Stores survey results to Datastore
+function surveyResults(currentUser, userID) {
     console.log("in")
     let Question1 = document.querySelector('input[name="yes/no1"]:checked').value;
     let checkboxes1 = document.querySelectorAll('input[name="symptoms1"]:checked');
-    let values1 = [];   
+    let values1 = [];
     checkboxes1.forEach((checkbox) => {
         values1.push(checkbox.value);
     });
@@ -29,7 +30,8 @@ function surveyResults() {
 
             currentUser.get()
                 .then(userDoc => {
-                    var userEmail = userDoc.data().email;
+                    // var userEmail = userDoc.data().email;
+                    console.log(userID);
                     db.collection("Results").add({
                         userID: userID,
                         close_contact: Question1,
@@ -60,27 +62,79 @@ function surveyResults() {
                         })
 
                     }).then(() => {
-                        //A thank-you message shows up
+                        //A thank-you message displays
                         let div = document.getElementById("survey-container");
 
-                        str = "<div id='message-after-submission'><h1 class='mx-3 my-5 text-center'>Thanks for your submission!</h1>"
-                        + "<div class='d-grid gap-2 d-sm-flex justify-content-sm-center text-center w-75 mx-auto'>"
-                        + "<button type='button' class='btn btn-lg btn-secondary mx-auto my-3' style='background-color: ; max-width: fit-content; font-size: 25px'><a href='./survey.html'"
-                        + "class='text-light text-decoration-none'>Survey again</a></button>"
-                        + "<button type='button' class='btn btn-lg mx-auto my-3' style='background-color: #F74F20; max-width: fit-content; font-size: 25px'><a href='./personalHealth.html'"
-                        + "class='text-light text-decoration-none'>See Result</a></button>"
-                        + "<button type='button' class='btn btn-lg mx-auto my-3 btn-success' style='max-width: fit-content; font-size: 25px'><a href='./main.html'"
-                        + "class='text-light text-decoration-none'>Back To Home</a></button></div></div>"
-                        + "<div id='thank-you-image'><img src='/images/thank-you.jpg' width='100%'/></div>";
-                        
+                        str = "<div id='message-after-submission'><h1 class='mx-3 my-5 text-center'>Thanks for your submission!</h1>" +
+                            "<div class='d-grid gap-2 d-sm-flex justify-content-sm-center text-center w-75 mx-auto'>" +
+                            "<button type='button' class='btn btn-lg btn-secondary mx-auto my-3' style='background-color: ; max-width: fit-content; font-size: 25px' onclick='surveyAgain()'><a class='text-light text-decoration-none'>Survey again</a></button>" +
+                            "<button type='button' class='btn btn-lg mx-auto my-3' style='background-color: #F74F20; max-width: fit-content; font-size: 25px'><a href='./personalHealth.html' class='text-light text-decoration-none'>See Result</a></button>" +
+                            "<button type='button' class='btn btn-lg mx-auto my-3 btn-success' style='max-width: fit-content; font-size: 25px'><a href='./main.html' class='text-light text-decoration-none'>Back To Home</a></button></div></div>" +
+                            "<div id='thank-you-image'><img src='/images/thank-you.jpg' width='100%'/></div>";
+
                         div.innerHTML = str;
                     })
-                        
+
+                })
+        }
+    })
+
+};
+
+//Check if user has done survey today
+function hasSurveyCompletedToday() {
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            var currentUser = db.collection("users").doc(user.uid)
+            var userID = user.uid;
+            console.log(userID);
+            currentUser.get()
+                .then(userDoc => {
+                    var surveyCompleted = userDoc.data().surveyCompleted;
+                    var lastUpdated = userDoc.data().survey_timestamp;
+                    var currentTime = firebase.firestore.Timestamp.fromDate(new Date());
+                    console.log("surveyCompleted " + surveyCompleted);
+                    console.log("survey last updated: " + lastUpdated.toDate());
+                    console.log("current time " + currentTime);
+                    // console.log((new Date()).getHours());
+                    if (surveyCompleted == "True") {
+                        if (Math.abs(lastUpdated.seconds - currentTime.seconds) < 86400) {
+                            console.log("user has done survey today");
+                            let div = document.getElementById("survey-container");
+
+                            str = "<div id='message-after-submission'><h1 class='mx-3 my-5 text-center'>You have done your survey today!</h1>" +
+                                "<div class='d-grid gap-2 d-sm-flex justify-content-sm-center text-center w-75 mx-auto'>" +
+                                "<button type='button' class='btn btn-lg btn-secondary mx-auto my-3' style='background-color: ; max-width: fit-content; font-size: 25px' onclick='surveyAgain()'><a class='text-light text-decoration-none'>Survey again</a></button>" +
+                                "<button type='button' class='btn btn-lg mx-auto my-3' style='background-color: #F74F20; max-width: fit-content; font-size: 25px'><a href='./personalHealth.html' class='text-light text-decoration-none'>See Result</a></button>" +
+                                "<button type='button' class='btn btn-lg mx-auto my-3 btn-success' style='max-width: fit-content; font-size: 25px'><a href='./main.html' class='text-light text-decoration-none'>Back To Home</a></button></div></div>";
+
+                            div.innerHTML = str;
+
+                        }
+                    } else {
+                        console.log("user must do survey");
+                        surveyResults();
+                    }
                 })
         } else {
             console.log("No user is sign in");
         }
+    });
+};
+
+//Do survey again
+function surveyAgain() {
+    console.log("do survey again");
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            var currentUser = db.collection("users").doc(user.uid)
+            var userID = user.uid;
+            currentUser.update({
+                surveyCompleted: "False"
+            }).then(() => {
+                window.location.reload();
+            })
+            
+        }
     })
-}
-
-
+};
