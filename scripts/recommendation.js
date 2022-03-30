e5852364d2f9ae00e7cf502faebd8e31e807ef52
Gareth Ng <ngys.gareth@gmail.com>
2022-03-30 15:46:29 -0700

    Gareth, recommendation based on survey results function completed

--- scripts/recommendation.js
+++ scripts/recommendation.js
@@ -19,87 +19,93 @@ firebase.auth().onAuthStateChanged(user => {
 
 
 function read_display_Recommendation(currentUser, userTest_ID) {
-    
-    var currentResults = db.collection("Results")
-    
+
+    var currentResults = db.collection("Results");
+    var resultsHigh = db.collection("Recommendation").doc("High");
+    var resultsMedium = db.collection("Recommendation").doc("Medium");
+    var resultsLow = db.collection("Recommendation").doc("Low");
+    var arraySymptoms1 = [];
+    var arraySymptoms2 = [];
+    var listOne;
+    var listTwo;
+
     console.log(userTest_ID);
-    
-
     currentResults.get()
-    .then(snap => {
-        snap.forEach(userDoc => {
-            var documentID = userDoc.id; //get the id of a survey result document
-            console.log(documentID);
-            db.collection("Results").doc(documentID).get() // get data of this document
-                .then(resultDoc => {
-                    var resultsUserID = resultDoc.data().userID;
-                    
-                    if (resultsUserID == userTest_ID) {
-                        db.collection("Results").doc(documentID).get()
-                            .then(thisDoc => {
-                                console.log("Where is this data?" + thisDoc.data().cough);
-                            })
-                    }
-                })
+        .then(snap => {
+            snap.forEach(userDoc => {
+                var documentID = userDoc.id; //get the id of a survey result document
+                console.log(documentID);
+                db.collection("Results").doc(documentID).get() // get data of this document
+                    .then(resultDoc => {
+                        var resultsUserID = resultDoc.data().userID;
+
+                        if (resultsUserID == userTest_ID) {
+                            db.collection("Results").doc(documentID).get()
+                                .then(thisDoc => {
+
+                                    arraySymptoms1.push(thisDoc.data().fever_chills);
+                                    arraySymptoms1.push(thisDoc.data().cough);
+                                    arraySymptoms1.push(thisDoc.data().shortness_breath);
+                                    arraySymptoms1.push(thisDoc.data().lost_sense_smell_taste);
+
+                                    arraySymptoms2.push(thisDoc.data().sorethroat);
+                                    arraySymptoms2.push(thisDoc.data().headache);
+                                    arraySymptoms2.push(thisDoc.data().fatigue_tiredness);
+                                    arraySymptoms2.push(thisDoc.data().runnynose);
+                                    arraySymptoms2.push(thisDoc.data().sneezing);
+                                    arraySymptoms2.push(thisDoc.data().diarrhea);
+                                    arraySymptoms2.push(thisDoc.data().lost_appetite);
+                                    arraySymptoms2.push(thisDoc.data().nausea_vomiting);
+                                    arraySymptoms2.push(thisDoc.data().body_muscle_aches);
+
+                                    for (i = 0; i < arraySymptoms1.length; i++) {
+                                        if (arraySymptoms1[i] = "true") {
+                                            listOne++;
+                                        }
+                                    }
+
+                                    for (i = 0; i < arraySymptoms2.length; i++) {
+                                        if (arraySymptoms2[i] = "true") {
+                                            listTwo++;
+                                        }
+                                    }
+
+                                    if (thisDoc.data().emergency == "yes") {
+                                        resultsHigh.get()
+                                            .then(highDoc => {
+                                                console.log(highDoc.data());
+                                                document.getElementById("recommendationGroup").innerHTML =
+                                                    "Instruction: " + highDoc.data().Instruction + "<br>" +
+                                                    "Details: " + highDoc.data().Details + "<br>";
+                                            })
+
+                                    } else if (thisDoc.data().close_contact == "yes" || listOne > 1 || listTwo > 3) {
+                                        resultsMedium.get()
+                                            .then(mediumDoc => {
+                                                console.log(mediumDoc.data());
+                                                document.getElementById("recommendationGroup").innerHTML =
+                                                    "Instruction: " + mediumDoc.data().Instruction + "<br>" +
+                                                    "Details: " + mediumDoc.data().Details + "<br>";
+                                            })
+                                    } else {
+                                        resultsLow.get()
+                                            .then(lowDoc => {
+                                                console.log(lowDoc.data());
+                                                document.getElementById("recommendationGroup").innerHTML =
+                                                    "Instruction: " + lowDoc.data().Instruction + "<br>" +
+                                                    "Details: " + lowDoc.data().Details + "<br>";
+                                            })
+                                    }
+
+
+                                })
+                        }
+
+                    })
+            })
         })
-    })
-    // currentUser.get()
-    // .then(testUser => {
-    //     testUser.
-    // })
-
-
-
-
-    // let usertest = localStorage.getItem("values1");
-    // console.log(usertest);
-    // db.collection("Results").where("userID", "==", usertest).get()
-    // .then(testDoc => {
-    //     console.log("This is it" + testDoc);
-    //     let array1 = [];
-    //     console.log(testDoc.data().cough);
-    // })
-    // db.collection("users").doc(user.uid).get()
-    // .then(() => {
-        
-    // })
-        
-            
-            // var results = db.collection("Results").get().then(allResults => {
-            //     allResults.forEach(doc => {
-            //         .where("user")
-            //     })
-            // })
-
-
-            
-        
-
-    
-    
-
-    // if (this.Question1 == "yes") {
-    //     db.collection("Recommendation").doc("High")
-    //     .get()
-    //     .then(highDoc => {
-    //         console.log(highDoc.data());
-    //         document.getElementById("recommendationGroup").innerHTML =
-    //         "Instruction: " + highDoc.data().Instruction + "<br>" +
-    //         "Details: " + highDoc.data().Details + "<br>";
-    //     })
-    // } else if (Question1 == "no") {
-    //     db.collection("Recommendation").doc("Medium")
-    //     .get()
-    //     .then(mediumDoc => {
-    //         console.log(mediumDoc.data());
-    //         document.getElementById("recommendationGroup").innerHTML = 
-    //         "Instruction: " + mediumDoc.data().Instruction + "<br>" +
-    //         "Details: " + mediumDoc.data().Details + "<br>";
-    //     })
-
-    // }
-
 }
+
 
 function insertName() {
     currentUser.get().then(userDoc => {
@@ -107,5 +113,6 @@ function insertName() {
         var user_Name = userDoc.data().name;
         console.log(user_Name);
         document.getElementById("name-goes-here").innerText = user_Name
+
     })
 }
