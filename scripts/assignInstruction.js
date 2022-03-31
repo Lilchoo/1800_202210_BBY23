43c20857067c3a5dfdb34f7ab91f0e7002a31a35
Linh Nguyen <knguyen142@my.bcit.ca>
2022-03-30 19:21:20 -0700

    Linh - assignInstruction page is WIP

--- scripts/assignInstruction.js
+++ scripts/assignInstruction.js
@@ -15,8 +15,10 @@ function showInfo() {
             if (userID == id) {
                 var role = doc.data().role;
                 var email = doc.data().email;
+                console.log("role: " + role + "\nemail: " + email);
                 let str = "role: " + role + "<br>email: " + email;
                 document.getElementById("info-goes-here").innerHTML = str;
+                getHealthInfo(userID);
             }
         })
     })
@@ -25,3 +27,37 @@ function showInfo() {
 
 }
 showInfo();
+
+
+function getHealthInfo(userID) {
+    console.log("getHealthInfo function");
+    var currentResult = db.collection("Results");
+    currentResult.get()
+        .then(snap => {
+            snap.forEach(resultDoc => {
+                var documentID = resultDoc.id;
+                currentResult.doc(documentID).get()
+                    .then(document => {
+                        var id = document.data().userID;
+                        // console.log(id);
+                        if (id == userID) {
+                            var currentDocID = document.id;
+                            console.log("found the result document with id: " + currentDocID);
+                            let currentDoc = document.data();
+                            values=Object.values(currentDoc);
+                            keys=Object.keys(currentDoc);
+                            let str = "";
+                            for (let i = 0; i < values.length; i++) {
+                                if (keys[i]=="close_contact" &&  values[i]== "yes") {
+                                    str += "Has close contact with "
+                                }
+                                if (values[i]) {
+                                    console.log(keys[i]);
+                                    return 
+                                }
+                            }
+                        }
+                    })
+            })
+        })
+}
