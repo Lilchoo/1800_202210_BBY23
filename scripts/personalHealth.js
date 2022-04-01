02fafe58754466f516182d172d2ad5d8199dc822
Gareth Ng <ngys.gareth@gmail.com>
2022-04-01 15:42:55 -0700

    Gareth added color change on personalHealth, currently in words but will change to a square color soon

--- scripts/personalHealth.js
+++ scripts/personalHealth.js
@@ -20,6 +20,10 @@ function Recommendations() {
 
 function insertSymptoms(currentUser, userTest_ID) {
     var currentResults = db.collection("Results");
+    var arraySymptoms1 = [];
+    var arraySymptoms2 = [];
+    var listOne;
+    var listTwo;
 
     console.log(userTest_ID);
     currentResults.get()
@@ -34,6 +38,44 @@ function insertSymptoms(currentUser, userTest_ID) {
                 if (resultsUserID == userTest_ID) {
                     db.collection("Results").doc(documentID).get()
                     .then(thisDoc => {
+
+                        arraySymptoms1.push(thisDoc.data().fever_chills);
+                        arraySymptoms1.push(thisDoc.data().cough);
+                        arraySymptoms1.push(thisDoc.data().shortness_breath);
+                        arraySymptoms1.push(thisDoc.data().lost_sense_smell_taste);
+
+                        arraySymptoms2.push(thisDoc.data().sorethroat);
+                        arraySymptoms2.push(thisDoc.data().headache);
+                        arraySymptoms2.push(thisDoc.data().fatigue_tiredness);
+                        arraySymptoms2.push(thisDoc.data().runnynose);
+                        arraySymptoms2.push(thisDoc.data().sneezing);
+                        arraySymptoms2.push(thisDoc.data().diarrhea);
+                        arraySymptoms2.push(thisDoc.data().lost_appetite);
+                        arraySymptoms2.push(thisDoc.data().nausea_vomiting);
+                        arraySymptoms2.push(thisDoc.data().body_muscle_aches);
+
+                        for (i = 0; i < arraySymptoms1.length; i++) {
+                            if (arraySymptoms1[i] = "true") {
+                                listOne++;
+                            }
+                        }
+
+                        for (i = 0; i < arraySymptoms2.length; i++) {
+                            if (arraySymptoms2[i] = "true") {
+                                listTwo++;
+                            }
+                        }
+
+                        if(thisDoc.data().emergency == "yes") {
+                            document.getElementById("health-status").innerHTML =
+                            "<h2>" + "red" + "</h2>"
+                        } else if(thisDoc.data().close_contact == "yes" || listOne > 1 || listTwo > 3) {
+                            document.getElementById("health-status").innerHTML =
+                            "<h2>" + "orange" + "</h2>"
+                        } else {
+                            document.getElementById("health-status").innerHTML =
+                            "<h2>" + "green" + "</h2>"
+                        }
 
                         document.getElementById("table-symptoms").innerHTML = 
                         "Fever and Chills: " + thisDoc.data().fever_chills + "<br>" +
