ad348cf489f6d98e4d5d5adc8742a752c019ad87
Avnoor Rocks <noorbhangu28@gmail.com>
2022-04-01 21:43:06 +0530

    Avnoor added cancel button to instruction.html, created schedule.hml and schedule.js

---
+++ scripts/schedule.js
@@ -0,0 +1,32 @@
+function read_display_Recommendation() {
+
+    db.collection("instructions").doc("user.uid")
+    .get()
+    .then(userDoc => {
+        console.log(user.uid);
+        document.getElementById("scheduleGroup").innerHTML = 
+        "Date: " + user.uid.date + "<br>" +
+        "Task: " + user.uid.instructions + "<br>";
+    })
+
+}
+
+function insertName() {
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {                                                       
+            console.log(user.uid);
+            currentUser = db.collection("users").doc(user.uid);
+            currentUser.get()
+                  .then(userDoc => {
+               var user_Name = userDoc.data().name;
+               console.log(user_Name);
+               var t = userDoc.data().timestamp;
+               console.log(t);
+               $("#name-goes-here").text(user_Name);
+            })
+        } else {
+            // No user is signed in.
+        }
+    });
+}
+insertName();
