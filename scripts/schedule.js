7b567f93f6dd7755b723336bc792d75b2b8aa71b
Avnoor Rocks <noorbhangu28@gmail.com>
2022-04-02 06:05:39 +0530

    Avnoor added schedule.js

--- scripts/schedule.js
+++ scripts/schedule.js
@@ -1,33 +1,40 @@
-function read_display_Instruction() {
+var currentUser;
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        currentUser = db.collection("users").doc(user.uid);
+        var userTest_ID = user.uid
+        console.log(userTest_ID);
 
-    db.collection("instructions")
-   .get()
+        insertInstruction(currentUser, userTest_ID);
+        insertName(currentUser);
+
+    } else {
+        // No user is signed in.
+
+    }
+})
+
+function insertInstruction(currentUser, userTest_ID) {
+    //currentUser = db.collection("users").doc(user.uid);
+    currentUser.get()
     .then(userDoc => {
-        console.log(user.uid);
-        document.getElementById("scheduleGroup").innerHTML = 
-        "Date: " + doc.data().date;
-        "Task: " + doc.data().instructions;
+
+        var date = userDoc.data().date;
+        console.log(date);
+        document.getElementById("date-goes-here").innerText = date
+        var task = userDoc.data().instructions;
+        console.log(task);
+        document.getElementById("instruction-goes-here").innerText = task
     })
-
 }
-read_display_Instruction();
 
-function insertName() {
-    firebase.auth().onAuthStateChanged(user => {
-        if (user) {                                                       
-            console.log(user.uid);
-            currentUser = db.collection("users").doc(user.uid);
-            currentUser.get()
-                  .then(userDoc => {
-               var user_Name = userDoc.data().name;
-               console.log(user_Name);
-               var t = userDoc.data().timestamp;
-               console.log(t);
-               $("#name-goes-here").text(user_Name);
-            })
-        } else {
-            // No user is signed in.
-        }
-    });
+
+function insertName(currentUser) {
+    currentUser.get().then(userDoc => {
+
+        var user_Name = userDoc.data().name;
+        console.log(user_Name);
+        document.getElementById("name-goes-here").innerText = user_Name
+
+    })
 }
-insertName();
